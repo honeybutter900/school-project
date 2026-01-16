4be75344754fa9d6f787509fed95e70e207d9fe6
honeybutter <honeybutter900@gmail.com>
2026-01-17 00:42:03 +0900

    Add header, main, and community components; update styles and images

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+function Header() {
+  return (
+    <header className='project-header'>
+      <div className='header-inner'>
+        <div className='logo'>
+          <a href='#'>
+            <img src='./images/logo.png' alt='로고'/>
+          </a>
+        </div>
+        <nav>
+          <ul>
+            <li><a href='#'>멘토/멘티</a></li>
+            <li><a href='#'>커뮤니티</a></li>
+            <li><a href='#'>캘린더</a></li>
+            <li><a href='#'>마이페이지</a></li>
+          </ul>
+        </nav>
+        <div className='user'>
+          <a href='#' className='login'>로그인</a>/
+          <a href='#' className='new-user'>회원가입</a>
+        </div>
+        </div>
+    </header>
+    );
+}
+
+export default Header;
---
+++ src/pages/Community.jsx
@@ -0,0 +1,10 @@
+function Community() {
+    return (
+        <div className="Community-header">
+            <input type="text" value='검색어를 입력해세요' />
+        </div>
+        
+    );
+}
+
+export default Community;
